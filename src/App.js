e557ab6593a14ab89a2ebf2526c9ed7498c13724
golden-state0fmind <areyes2009@yahoo.com>
2021-03-15 13:03:06 -0700

    adds firebase/firestore with add function to db collections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { firebase, db } from './firebase'
+
+db.collection("test from module").add({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+})
+.then((docRef) => {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+  console.error("Error adding document: ", error);
+});
 
 function App() {
   return (
