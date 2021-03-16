56fb0f4b4bc91b8fd94c53c103c1194070d46319
golden-state0fmind <areyes2009@yahoo.com>
2021-03-16 03:52:17 -0700

    adds tailwind styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+  <div class="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
+  <p class="text-black-600 group-hover:text-gray-900 ...">Home</p>
+  <p class="text-green-500 group-hover:text-gray-500 ...">Welcome Roomie!</p>
+</div>
+  )
 };
 
 export default Home;
