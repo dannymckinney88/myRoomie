7bf817e0b013e54a18c4eed875c6e2245db183c2
dannymckinney88 <dannymckinney88@gmail.com>
2021-03-17 14:46:34 -0700

    Center Hero Img and Welcome text/sign up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,40 @@ import { ReactComponent as Logo } from "../assets/myRoomie-logo/vector/default-m
 import img from "../assets/team-at-work.png";
 const Home = () => {
   return (
-    <div>
-      <nav class="group  my-5 border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
+    <div class="font-serif bg-green-100 h-screen	">
+      <nav class="group  hover:bg-white hover:shadow-lg hover:border-transparent ...">
         <p class="text-black-600 group-hover:text-gray-900 float-right px-3">
           Log In
         </p>
-        <p class="text-black-600 group-hover:text-gray-900 float-right px-3">
-          sign up
-        </p>
-        <img src="/src/assets/process-outline.png" alt="" />
+        <p class="float-right px-3">sign up</p>
         <Logo class="w-20 ml-3" />
       </nav>
       <div class="container mx-auto">
-        <div class="grid grid-cols-2 h-screen mt-80">
-          <div class="">
-            <h1>myRoomie helping conect shared lilving easier and covenient</h1>
-          </div>
-          <div class="">
-            <img src={img} alt="" class="max-w-3xl " />
+        <div className="row">
+          <div class="grid grid-cols-2  mt-40">
+            <div class="break-words">
+              <h1 class="text-4xl font-bold">
+                myRoomie helping conect shared lilving, easier and covenient.
+              </h1>
+              <p class="font-medium mt-6">
+                Invite roomates, signficant others, or anyone living together,
+                and make living together a breeze. From spliting bills to
+                dividing chores and place to track all of it conveniently.
+              </p>
+              <form action="" class="row my-5">
+                <div class="d-none d-md-block col-md-7">
+                  <input type="email" class="form-control h-100" />
+                </div>
+              </form>
+            </div>
+            <div>
+              <img
+                src={img}
+                alt=""
+                class="max-w-3xl "
+                style={{ height: "300px" }}
+              />
+            </div>
           </div>
         </div>
       </div>
