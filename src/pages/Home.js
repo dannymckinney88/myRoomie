48d374cb08ecaa5899e65cb1ddda092499076447
golden-state0fmind <areyes2009@yahoo.com>
2021-03-19 22:03:49 -0700

    cleans up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
+import React from 'react'
+
 const Home = () => {
   return (
-  <div class="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
-  <p class="text-black-600 group-hover:text-gray-900 ...">Home</p>
-  <p class="text-green-500 group-hover:text-gray-500 ...">Welcome Roomie!</p>
-</div>
+    <div className="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
+      <p className="text-black-600 group-hover:text-gray-900 ...">Home</p>
+      <p className="text-green-500 group-hover:text-gray-500 ...">Welcome Roomie!</p>
+    </div>
   )
 };
 
