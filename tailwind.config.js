403919272c61da69e3e58eb2f8cb5f81c6d77212
dannymckinney88 <dannymckinney88@gmail.com>
2021-03-22 16:42:36 -0700

    Adds background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-su": "url('/src/assets/signup-bg.jpg')",
+      },
+    },
     screens: {
       tablets: "640px", // => @media (min-width: 640px)
       laptop: "1024px", // => @media (min-width: 1024px)
