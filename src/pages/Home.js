a11a8e0552b2fce26a18a5981015a60cccc6a918
dannymckinney88 <dannymckinney88@gmail.com>
2021-03-22 16:43:16 -0700

    Removes un needed code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ReactComponent as Logo } from "../assets/myRoomie-logo/vector/default-monochrome.svg";
 import img from "../assets/team-at-work.png";
 const Home = () => {
@@ -12,8 +11,8 @@ const Home = () => {
         <Logo class="w-20 ml-3" />
       </nav>
       <div class="container mx-auto ">
-        <div class=" mt-40  grid grid-cols-2 sm:grid-cols-1 gap-10">
-          <div class="break-words ">
+        <div class=" mt-60 flex flex-wrap  lg:-mx-4">
+          <digit v class="break-words w-1/2 overflow-hidden lg:my-4 lg:px-4">
             <h1 class="text-4xl font-bold">
               myRoomie helping conect shared lilving, easier and covenient.
             </h1>
@@ -31,30 +30,19 @@ const Home = () => {
               </div>
               <div className="col-md-5 pl-md-2"></div>
             </form>
-          </div>
-          <div class="w-1/2  ">
+          </digit>
+          <div class="w-1/2  lg:my-4 lg:px-2">
             <img
               src={img}
               alt="Team at work paper img"
-              class="object-fill max-w-4xl"
-              // style={{ height: "90%" }}
+              class=""
+              style={{ height: "60%" }}
             />
           </div>
         </div>
       </div>
     </div>
   );
-=======
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div className="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
-      <p className="text-black-600 group-hover:text-gray-900 ...">Home</p>
-      <p className="text-green-500 group-hover:text-gray-500 ...">Welcome Roomie!</p>
-    </div>
-  )
->>>>>>> main
 };
 
 export default Home;
