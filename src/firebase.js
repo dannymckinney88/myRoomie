2e4b56da86c824c1c8522c3b0385ebd9528d45ee
golden-state0fmind <areyes2009@yahoo.com>
2021-03-22 20:14:02 -0700

    changes settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase';
 import 'firebase/firestore';
 import "firebase/auth"
 
-require('dotenv').config()
+//require('dotenv').config()
 require('firebase/firestore')
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
+    apiKey: 'AIzaSyBK9mB9z57qLTOKXS-T9EgZzSJhT0PigiI',
+    authDomain: 'myroomie-2f3c7.firebaseapp.com',
+    databaseURL: 'https://myroomie-2f3c7-default-rtdb.firebaseio.com',
     projectId: "myroomie-2f3c7",
 };
 
@@ -16,4 +16,17 @@ firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth()
 export const db = firebase.firestore(); // NEW
-export default {firebase, firebaseConfig}
+export default {firebase, firebaseConfig, auth}
+
+export const SignUp = (email, password) => {
+    //console.log('inside the SIGNUP FUNCTION export')
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        let user = userCredential.user;
+    })
+    .catch((error) => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+    })
+}
+
