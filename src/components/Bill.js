24df1c306d0de801d0a0b0488cda4312b01c371e
dannymckinney88 <dannymckinney88@gmail.com>
2021-03-25 16:32:28 -0700

    Builds out bill component and some basic styling

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
-import BillContainer from "./BillContainer";
+import BillItems from "./BillItems";
 export default function Bill() {
   const [bills] = useState([
     {
       name: "rent",
-      paidBy: ["Joey", "Antonio", "Tony"],
+      paidBy: [
+        { name: "Joey", paid: true },
+        { name: "Antonio", paid: false },
+        { name: "Tony", paid: false },
+      ],
       amount: 1300,
     },
   ]);
@@ -20,18 +24,60 @@ export default function Bill() {
   const bill = bills.map((billInfo) => (
     <>
       <div>
-        <h4>{billInfo.name}</h4>
+        <h2 className="text-left text-2xl font-bold uppercase">
+          {billInfo.name}
+        </h2>
       </div>
-      <div>
-        {billInfo.paidBy.map((name) => (
-          <>
-            <BillContainer
-              name={name}
-              amount={billInfo.amount / billInfo.paidBy.length}
-            />
-          </>
-        ))}
-      </div>
+      <table class="table-auto shadow-lg bg-white">
+        <thead> {/* <h2 className="text-center">{billInfo.name}</h2> */}</thead>
+        <thead>
+          <tr>
+            <th>Roomie</th>
+            <th>Amount</th>
+            <th>Paid</th>
+          </tr>
+        </thead>
+        <tbody>
+          {billInfo.paidBy.map((person) => (
+            <>
+              <BillItems
+                name={person.name}
+                paid={person.paid}
+                amount={billInfo.amount / billInfo.paidBy.length}
+              />
+            </>
+          ))}
+        </tbody>
+      </table>
+      {/* <table class="table-auto shadow-lg bg-white">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Views</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border px-8 py-4">Intro to CSS</td>
+              <td className="border px-8 py-4">Adam</td>
+              <td className="border px-8 py-4">858</td>
+            </tr>
+            <tr class="bg-emerald-200">
+              <td>
+                A Long and Winding Tour of the History of UI Frameworks and
+                Tools and the Impact on Design
+              </td>
+              <td>Adam</td>
+              <td>112</td>
+            </tr>
+            <tr>
+              <td>Intro to JavaScript</td>
+              <td>Chris</td>
+              <td>1,280</td>
+            </tr>
+          </tbody>
+        </table> */}
     </>
   ));
 
