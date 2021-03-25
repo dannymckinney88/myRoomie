f93ab56d1cb316393e17f7c675703d1e19417152
dannymckinney88 <dannymckinney88@gmail.com>
2021-03-25 12:46:36 -0700

    Removes content and removes component from tree

--- src/components/TabsContainer.js
+++ src/components/TabsContainer.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { Tabs } from "@feuer/react-tabs";
-import TabContent from "./TabContent";
+import Bills from "./Bills";
+import Chores from "./Chores";
+import Dashboard from "./Dashboard";
 
 export default function Tab(props) {
   const [list] = useState(["Bills", "Chores"]);
+  const [bills, setBills] = useState(["bills"]);
+  const [chores, setChores] = useState(["chores"]);
   const styles = {
     fontFamily: "sans-serif",
     textAlign: "center",
   };
   console.log(props);
 
-  const tabs = list.map((tab) => <TabContent name={tab} />);
+  // const tabs = list.map((tab) => <TabContent name={tab} />);
   return (
     <div style={styles}>
       <Tabs
@@ -18,7 +22,30 @@ export default function Tab(props) {
           id: list[0],
         }}
       >
-        {tabs}
+        <Tabs.Tab id="Dashboard" title="Dashboard">
+          <div className="container pt-4">
+            <div>
+              <Dashboard />
+            </div>
+          </div>
+        </Tabs.Tab>
+        <Tabs.Tab id={chores[0]} title={chores[0]}>
+          <div className="container pt-4">
+            <div>
+              {chores.length > 0 ? <Chores /> : "No Bills"}
+              {/* <h1>{bills[0]}</h1> */}
+            </div>
+          </div>
+        </Tabs.Tab>
+        <Tabs.Tab id={bills[0]} title={bills[0]}>
+          <div className="container pt-4">
+            <div>
+              {bills.length > 0 ? <Bills /> : "No Bills"}
+              {/* <h1>{bills[0]}</h1> */}
+            </div>
+          </div>
+        </Tabs.Tab>
+        {/* {tabs} */}
       </Tabs>
     </div>
   );
