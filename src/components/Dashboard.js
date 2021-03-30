b49a007a6414bae3cb3482abf7fafdaa362289a2
golden-state0fmind <areyes2009@yahoo.com>
2021-03-29 21:37:49 -0700

    starts the use of the modal

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import Modal from './Modal'
 
 const Dashboard = () => {
-  return <div>DashBoard</div>;
+const [show,setShow]=useState(false)
+
+const showModal = (e) =>{
+    setShow(true)
+}
+
+const closeModal = (e) =>{
+  setShow(false)
+}
+
+  return (
+    <>
+      <div>DashBoard</div>
+        <button onClick={e => showModal()}>
+          show modal
+        </button>
+      <Modal showModal={show} closeModal={closeModal} >
+        <label>hello</label>
+        <input type="text" />
+        <label>moto</label>
+        <input type="text" />
+      </Modal>
+    </>
+  );
 };
 
 export default Dashboard;
