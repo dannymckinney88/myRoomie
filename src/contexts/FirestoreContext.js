7cab89c084e484488baee62169cbc4ee787ecc7a
dannymckinney88 <dannymckinney88@gmail.com>
2021-04-05 20:01:36 -0700

    Adds fetching bills and passing roomID

--- src/contexts/FirestoreContext.js
+++ src/contexts/FirestoreContext.js
@@ -11,6 +11,8 @@ export function FirestoreProvider({ children }) {
   const [rooms, setRooms] = useState([])
   const [room, setRoom] = useState("")
   const [roomsId, setRoomsId] = useState([])
+  const [roomId, setRoomId] = useState()
+  const [bills, setBills] = useState([])
   // Room API
   // - Writes
   const roomRef = db.collection("rooms")
@@ -30,21 +32,23 @@ export function FirestoreProvider({ children }) {
 
   // -- Reads
   const fetchRooms = async (uid) => {
-    await db
-      .collection("rooms")
-      .where("userIds", "array-contains", uid)
-      .get()
-      .then((snapshot) => {
-        console.log(uid)
-        snapshot.forEach((doc) => {
-          console.log(doc.id)
-          setRooms((oldArray) => [...oldArray, doc.data()])
-          setRoomsId((oldArray) => [...oldArray, doc.id])
+    if (rooms.length < 1) {
+      await db
+        .collection("rooms")
+        .where("userIds", "array-contains", uid)
+        .get()
+        .then((snapshot) => {
+          console.log(uid)
+          snapshot.forEach((doc) => {
+            console.log(doc.id)
+            setRooms((oldArray) => [...oldArray, doc.data()])
+            setRoomsId((oldArray) => [...oldArray, doc.id])
+          })
         })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
   }
 
   const fetchRoom = async (roomId) => {
@@ -55,18 +59,50 @@ export function FirestoreProvider({ children }) {
         if (doc.exists) {
           console.log("doc Data :", doc.data())
           setRoom(doc.data())
+          setRoomId(doc.id)
+          console.log(doc.id)
         }
       })
   }
+
+  // All bills for a room
+  const fetchBills = async () => {
+    if (bills.length < 1) {
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("Bills")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            setBills((oldArray) => [...oldArray, doc.data()])
+            console.log(doc.id, "+>", doc.data())
+          })
+        })
+    }
+  }
+
+  // Fetchs all bills of a spefic user
+  // const fetchBills = async () => {
+  //   db.collectionGroup("Bills")
+  //     .where("users", "array-contains", "Danny")
+  //     .get()
+  //     .then((snapshot) => {
+  //       snapshot.forEach((doc) => {
+  //         console.log(doc.id, "=>", doc.data())
+  //       })
+  //     })
+  // }
 
   const value = {
     addRoom,
     addUserSub,
     rooms,
     roomsId,
+    roomId,
     fetchRooms,
     room,
     fetchRoom,
+    fetchBills,
   }
 
   return (
