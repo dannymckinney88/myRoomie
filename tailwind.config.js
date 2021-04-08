90196270c5262936880f20fc77a8c4a9fb88b604
dannymckinney88 <dannymckinney88@gmail.com>
2021-04-08 14:34:51 -0700

    Fixes prefixes for screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require("tailwindcss/plugin")
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
@@ -9,13 +9,14 @@ module.exports = {
       },
     },
     screens: {
-      tablets: "640px", // => @media (min-width: 640px)
-      laptop: "1024px", // => @media (min-width: 1024px)
-      desktop: "1280px", // => @media (min-width: 1280px)
+      sm: "640px", // => @media (min-width: 640px)
+      md: "768px", // => @media (min-width: 1024px)
+      lg: "1024px", // => @media (min-width: 1280px)
+      xl: "1280px",
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
