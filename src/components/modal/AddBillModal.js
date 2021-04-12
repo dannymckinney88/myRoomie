cc7f68868a9c15fca528c93d929b0b21d68e5b22
dannymckinney88 <dannymckinney88@gmail.com>
2021-04-11 22:47:57 -0700

    Trying to redirect modal

--- src/components/modal/AddBillModal.js
+++ src/components/modal/AddBillModal.js
@@ -1,12 +1,15 @@
 import React, { useState, useRef, useEffect } from "react"
 import Modal from "./Modal"
+import { Redirect } from "react-router-dom"
 import { useFirestore } from "../../contexts/FirestoreContext"
+import { db } from "../../firebase"
 
-export default function AddBillModal() {
+export default function AddBillModal(props) {
   const modal = useRef(null)
   const [billName, setBillName] = useState("")
   const [billAmount, setBillAmount] = useState()
   const [checkBoxOptions, setCheckBoxOptions] = useState([])
+  const [redirect, setRedirect] = useState(false)
 
   const { room, addBill } = useFirestore()
 
@@ -19,7 +22,31 @@ export default function AddBillModal() {
   }
   const handleBill = async (e) => {
     e.preventDefault()
-    addBill(billName, billAmount, checkBoxOptions)
+    const users = []
+    checkBoxOptions.forEach((user) => {
+      console.log(user)
+      if (user.paid === true) {
+        users.push(user)
+      }
+    })
+    await db
+      .collection("rooms")
+      .doc(props.roomId)
+      .collection("Bills")
+      .add({
+        billName: billName,
+        totalAmount: billAmount,
+        users: users,
+      })
+      .then((doc) => {
+        console.log(doc.data)
+      })
+    // (billName, billAmount, checkBoxOptions).then((bill) => {
+    //   console.log(bill.data())
+    // })
+
+    setRedirect(true)
+    console.log(redirect)
     console.log(billName)
     console.log(billAmount, checkBoxOptions)
   }
@@ -46,92 +73,101 @@ export default function AddBillModal() {
   }, [room])
 
   return (
-    <div>
+    <>
+      {redirect && <Redirect to={`/room/${props.roomId}/${props.roomName}`} />}
       <div>
-        <button
-          className="bg-white text-black rounded-full py-3 px-6 mt-6 "
-          onClick={() => modal.current.open()}
-        >
-          {" "}
-          Open me
-        </button>
-      </div>
-      <Modal ref={modal}>
-        <form onSubmit={handleBill}>
-          <div>
-            <label
-              htmlFor="bill"
-              className="text-small font-bold text-white block my-1"
-            >
-              Bill
-            </label>
-            <input
-              onChange={handleBillName}
-              value={billName}
-              className="w-full p-2 border border-gray-300 rounded mb-3"
-              type="text"
-              id="bill"
-              name="bill"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="amount"
-              className="text-small font-bold text-white block my-1"
-            >
-              Amount $
-            </label>
-            <input
-              onChange={handleBillAmount}
-              value={billAmount}
-              className="w-full p-2 border border-gray-300 rounded mt-1 mb-3"
-              type="text"
-              id="amount"
-              name="amount"
-            />
-          </div>
-
-          <div className="flex flex-row">
-            {checkBoxOptions.length > 1
-              ? checkBoxOptions.map((option, index) => (
-                  <div
-                    className="flex flex-col justify-items-center justify-centers items-center p-4"
-                    key={index}
-                  >
-                    <label htmlFor="name">{option.name}</label>
-                    <input
-                      onChange={(event) => {
-                        let checked = event.target.checked
-                        setCheckBoxOptions(
-                          checkBoxOptions.map((data) => {
-                            if (option.id === data.id && data.paid === false) {
-                              data.paid = true
-                            } else if (
-                              option.id === data.id &&
-                              data.paid === true
-                            ) {
-                              data.paid = false
-                            }
-                            return data
-                          })
-                        )
-                      }}
-                      user={option.name}
-                      type="checkbox"
-                    />
-                  </div>
-                ))
-              : ""}
-          </div>
+        <div>
           <button
-            type="submit"
             className="bg-white text-black rounded-full py-3 px-6 mt-6 "
-            onClick={handleBill}
+            onClick={() => modal.current.open()}
           >
-            Add Room
+            {" "}
+            Open me
           </button>
-        </form>
-      </Modal>
-    </div>
+        </div>
+        <Modal ref={modal}>
+          <form onSubmit={handleBill}>
+            <div>
+              <label
+                htmlFor="bill"
+                className="text-small font-bold text-white block my-1"
+              >
+                Bill
+              </label>
+              <input
+                onChange={handleBillName}
+                value={billName}
+                className="w-full p-2 border border-gray-300 rounded mb-3"
+                type="text"
+                id="bill"
+                name="bill"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="amount"
+                className="text-small font-bold text-white block my-1"
+              >
+                Amount $
+              </label>
+              <input
+                onChange={handleBillAmount}
+                value={billAmount}
+                className="w-full p-2 border border-gray-300 rounded mt-1 mb-3"
+                type="text"
+                id="amount"
+                name="amount"
+              />
+            </div>
+
+            <div className="flex flex-row">
+              {checkBoxOptions.length > 1
+                ? checkBoxOptions.map((option, index) => (
+                    <div
+                      className="flex flex-col justify-items-center justify-centers items-center p-4"
+                      key={index}
+                    >
+                      <label htmlFor="name">{option.name}</label>
+                      <input
+                        onChange={(event) => {
+                          let checked = event.target.checked
+                          setCheckBoxOptions(
+                            checkBoxOptions.map((data) => {
+                              if (
+                                option.id === data.id &&
+                                data.paid === false
+                              ) {
+                                data.paid = true
+                              } else if (
+                                option.id === data.id &&
+                                data.paid === true
+                              ) {
+                                data.paid = false
+                              }
+                              return data
+                            })
+                          )
+                        }}
+                        user={option.name}
+                        type="checkbox"
+                      />
+                    </div>
+                  ))
+                : ""}
+            </div>
+            <button
+              type="submit"
+              className="bg-white text-black rounded-full py-3 px-6 mt-6 "
+              onClick={(e) => {
+                handleBill(e)
+                setRedirect(true)
+              }}
+            >
+              Add Room
+            </button>
+          </form>
+        </Modal>
+      </div>
+    </>
   )
 }
