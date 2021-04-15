f921fe3423872ef0281f9e5ce0d85cb48a7ed688
dannymckinney88 <dannymckinney88@gmail.com>
2021-04-15 16:33:40 -0700

    Testing input

---
+++ src/components/inputs/Input.js
@@ -0,0 +1,21 @@
+// import React, { useState } from "react"
+
+// export default function Input(props) {
+//   return (
+//     <div>
+//       <label
+//         htmlFor={props.name}
+//         className="text-small font-bold text-white block my-1"
+//       >
+//         {props.label}
+//       </label>
+//       <input
+//         className="w-full p-2 border border-gray-300 rounded mb-3"
+//         type={props.type}
+//         name={props.name}
+//         value={props.value}
+//         onChange={props.handleChage}
+//       />
+//     </div>
+//   )
+// }
